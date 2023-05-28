c6db2fcd467dd80ae1ff5f362aeeb0d0c2923637
Flip86gto <75633532+Flip86gto@users.noreply.github.com>
2023-05-27 21:56:54 -0500

    Started Implementing class in hand code

--- simulink/Data/Code_Gen_Model_data.m
+++ simulink/Data/Code_Gen_Model_data.m
@@ -1,3 +1,6 @@
 % sample time model
 t_sample = .02;
-CONST('m_SparkMax_Channel', '30');
+CONST('Turn_Offset_FR', '0');
+CONST('Turn_Offset_FL', '0');
+CONST('Turn_Offset_BR', '0');
+CONST('Turn_Offset_BL', '0');
---
+++ simulink/Utilities/load_workspace.m
@@ -0,0 +1,8 @@
+% load code gen model data in base workspace
+evalin('base','Code_Gen_Model_data');
+
+% configuration file for code gen model
+evalin('base','load(''Code_Gen_Model_Config.mat'')');
+
+% load configurations
+% evalin('base','load(''Constants.sldd'')');
