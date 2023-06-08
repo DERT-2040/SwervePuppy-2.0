a86c44e25842c6d6c1d9b88d5e75e89f508675df
TheFlippedSuit <111720831+TheFlippedSuit@users.noreply.github.com>
2023-06-07 22:59:42 -0500

    Fixed the code to be joystick accurate, and importently remove the random spinning cuased by atan2 jumps.

--- simulink/Data/Code_Gen_Model_data.m
+++ simulink/Data/Code_Gen_Model_data.m
@@ -4,3 +4,9 @@ CONST('Turn_Offset_FR', '0');
 CONST('Turn_Offset_FL', '0');
 CONST('Turn_Offset_BR', '0');
 CONST('Turn_Offset_BL', '0');
+
+Steering_PID_prop = 1;
+Steering_PID_int = 0.02;
+Steering_PID_der = 0.0025;
+
+RIO_Offset = pi/2;
