3ca0065fb155647cd6662dd5098eca4fcf8ee165
TheFlippedSuit <111720831+TheFlippedSuit@users.noreply.github.com>
2023-06-08 21:18:54 -0500

    made changes in formatting and simplified the code. It made relative steering bypass the steering PID

--- simulink/Data/Code_Gen_Model_data.m
+++ simulink/Data/Code_Gen_Model_data.m
@@ -5,6 +5,21 @@ CONST('Turn_Offset_FL', '0');
 CONST('Turn_Offset_BR', '0');
 CONST('Turn_Offset_BL', '0');
 
+% modulation curve 
+temp_x = [0.1 0.2  0.3 0.4 0.5  0.6  0.7  0.8 0.9  1.0];
+temp_y = [0.0 0.04 0.1 0.2 0.31 0.43 0.55 0.7 0.85 1.0];
+% figure(1), clf, plot(temp_x,temp_y,'-o'), grid on
+% figure(2), clf, plot([-fliplr(temp_x) 0 temp_x],[-fliplr(temp_y) 0 temp_y],'-o'), grid on
+
+Steering_Mod_Drv_in = [-fliplr(temp_x) 0 temp_x];
+Steering_Mod_Drv_out = [-fliplr(temp_y) 0 temp_y];
+
+Steering_Mod_Str_Abs_in = [-fliplr(temp_x) 0 temp_x];
+Steering_Mod_Str_Abs_out = [-fliplr(temp_y) 0 temp_y];
+
+Steering_Mod_Str_Rel_in = [-fliplr(temp_x) 0 temp_x];
+Steering_Mod_Str_Rel_out = [-fliplr(temp_y) 0 temp_y];
+
 Steering_PID_prop = 1;
 Steering_PID_int = 0.02;
 Steering_PID_der = 0.0025;
