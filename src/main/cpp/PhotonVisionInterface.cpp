b26e31018ef373aafec3ee31237cb51749ce772d
TheFlippedSuit <111720831+TheFlippedSuit@users.noreply.github.com>
2023-11-13 22:08:38 -0600

    this should* be ready to be hooked up to the robot.

--- src/main/cpp/PhotonVisionInterface.cpp
+++ src/main/cpp/PhotonVisionInterface.cpp
@@ -1,6 +1,76 @@
 #include <include/PhotonVisionInterface.h>
 
-class PhotonVisionInterface {
-    public:
+const std::vector<double>& PhotonVisionInterface::getDistances() const{
+    return distances;
+}
 
+const std::vector<double>& PhotonVisionInterface::getYaws() const {
+    return yaws;
+}
+
+const std::vector<double>& PhotonVisionInterface::getPitches() const {
+    return pitches;
+}
+
+const std::vector<int>& PhotonVisionInterface::getFiducialIds() const {
+    return fiducialIds;
+}
+
+const std::vector<double>& PhotonVisionInterface::getPoseAmbiguities() const {
+    return poseAmbiguities;
+}
+
+const units::length::meter_t PhotonVisionInterface::determineTargetHeight(int fiducialId) const {
+    // Define a mapping or lookup for fiducial IDs to tag heights
+    std::unordered_map<int, units::length::meter_t> fiducialIdToHeight = {
+    //Target hights in meters for each corresponding id
+    {1, 0.462788_m}, 
+    {2, 0.462788_m},
+    {3, 0.462788_m},
+    {4, 0.695452_m},
+    {5, 0.695452_m},
+    {6, 0.462788_m},
+    {7, 0.462788_m},
+    {8, 0.462788_m}
 };
+
+    // Use the fiducial ID to look up the corresponding height
+    auto heightIterator = fiducialIdToHeight.find(fiducialId);
+    if (heightIterator != fiducialIdToHeight.end()) {
+        return heightIterator->second;
+    } else {
+        // Handle the case where the fiducial ID is not found
+        // You might want to return a default value, throw an exception, or handle it accordingly.
+        // Here, I'm returning a default value of 0 meters; replace it with your specific logic.
+        return 0.0_m;
+    }
+}
+
+void PhotonVisionInterface::updatePhotonVisionPipeline() {
+    photonlib::PhotonPipelineResult latestPipelineResult = camera.GetLatestResult();
+    bool hasTargets = latestPipelineResult.HasTargets();
+
+    // Clear the vectors to avoid double initialization
+    distances.clear();
+    yaws.clear();
+    pitches.clear();
+    fiducialIds.clear();
+    poseAmbiguities.clear();
+
+    if (hasTargets) {
+        const auto& photonVisionTrackedTargets = latestPipelineResult.GetTargets();
+
+        // Populate the vectors with data from each detected target
+        for (const auto& target : photonVisionTrackedTargets) {
+            distances.push_back(static_cast<double>(photonlib::PhotonUtils::CalculateDistanceToTarget(
+                CAMERA_HEIGHT, 
+                PhotonVisionInterface::determineTargetHeight(target.GetFiducialId()), 
+                CAMERA_PITCH, 
+                units::degree_t{target.GetPitch()})));
+            yaws.push_back(static_cast<double>(target.GetYaw()));
+            pitches.push_back(static_cast<double>(target.GetPitch()));
+            fiducialIds.push_back(static_cast<double>(target.GetFiducialId()));
+            poseAmbiguities.push_back(static_cast<double>(target.GetPoseAmbiguity()));
+        }
+    }
+}
--- src/main/include/PhotonVisionInterface.h
+++ src/main/include/PhotonVisionInterface.h
@@ -1,1 +1,37 @@
 #pragma once
+#include <photonlib/PhotonUtils.h>
+#include <photonlib/PhotonCamera.h>
+#include <unordered_map>
+
+class PhotonVisionInterface {
+public:
+    // This method should be called each tick of the Rio to send new data to Simulink
+    void updatePhotonVisionPipeline();
+    // Methods to return the vectors
+    const std::vector<double>& getDistances() const;
+
+    const std::vector<double>& getYaws() const;
+
+    const std::vector<double>& getPitches() const;
+
+    const std::vector<int>& getFiducialIds() const;
+    /**
+     * @brief Returns confidence factor [0, 1] of the targets
+    */
+    const std::vector<double>& getPoseAmbiguities() const;
+
+    const units::length::meter_t determineTargetHeight(int fiducialId) const;
+
+private:
+    // Makes PhotonCamera Object
+    photonlib::PhotonCamera camera{"photonvision"};
+    // Member variables to store data for each detected target
+    std::vector<double> distances;
+    std::vector<double> yaws;
+    std::vector<double> pitches;
+    std::vector<int> fiducialIds;
+    std::vector<double> poseAmbiguities;
+    // Constants with preset values
+    static constexpr units::meter_t CAMERA_HEIGHT{1.0}; // meters
+    static constexpr units::degree_t CAMERA_PITCH{1.0}; // degrees
+};
