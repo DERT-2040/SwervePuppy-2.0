5aeb8d69e5eb0f0a26aba0f0e0fac36d7bd2ae10
JoshA <andersonjoshua578@gmail.com>
2023-11-16 18:16:45 -0600

    saddafsf

--- src/main/cpp/HIDs.cpp
+++ src/main/cpp/HIDs.cpp
@@ -6,9 +6,9 @@
 
 void HIDs::PreStep() {
     Code_Gen_Model_U.Drive_Joystick_X = m_Drive_Joystick.GetX();
-    Code_Gen_Model_U.Drive_Joystick_Y = m_Drive_Joystick.GetY();
+    Code_Gen_Model_U.Drive_Joystick_Y = -m_Drive_Joystick.GetY();
     Code_Gen_Model_U.Steer_Joystick_X = m_Steer_Joystick.GetX();
-    Code_Gen_Model_U.Steer_Joystick_Y = m_Steer_Joystick.GetY();
+    Code_Gen_Model_U.Steer_Joystick_Y = -m_Steer_Joystick.GetY();
 }
 
 void HIDs::PostStep() {
