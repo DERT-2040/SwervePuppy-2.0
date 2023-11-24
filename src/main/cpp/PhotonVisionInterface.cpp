8e6649f01d0060a6c853046808196493d83a714c
TheFlippedSuit <111720831+TheFlippedSuit@users.noreply.github.com>
2023-11-24 17:36:32 -0600

    Made changes to use PhotonPoseEstimator

--- src/main/cpp/PhotonVisionInterface.cpp
+++ src/main/cpp/PhotonVisionInterface.cpp
@@ -1,5 +1,8 @@
 #include <include/PhotonVisionInterface.h>
 
+PhotonVisionInterface::PhotonVisionInterface() {
+}
+
 const std::vector<double>& PhotonVisionInterface::getDistances() const{
     return distances;
 }
@@ -20,33 +23,7 @@ const std::vector<double>& PhotonVisionInterface::getPoseAmbiguities() const {
     return poseAmbiguities;
 }
 
-const units::length::meter_t PhotonVisionInterface::determineTargetHeight(int fiducialId) const {
-    // Define a mapping or lookup for fiducial IDs to tag heights
-    std::unordered_map<int, units::length::meter_t> fiducialIdToHeight = {
-    //Target hights in meters for each corresponding id
-    {1, 0.462788_m}, 
-    {2, 0.462788_m},
-    {3, 0.462788_m},
-    {4, 0.695452_m},
-    {5, 0.695452_m},
-    {6, 0.462788_m},
-    {7, 0.462788_m},
-    {8, 0.462788_m}
-};
-
-    // Use the fiducial ID to look up the corresponding height
-    auto heightIterator = fiducialIdToHeight.find(fiducialId);
-    if (heightIterator != fiducialIdToHeight.end()) {
-        return heightIterator->second;
-    } else {
-        // Handle the case where the fiducial ID is not found
-        // You might want to return a default value, throw an exception, or handle it accordingly.
-        // Here, I'm returning a default value of 0 meters; replace it with your specific logic.
-        return 0.0_m;
-    }
-}
-
-void PhotonVisionInterface::updatePhotonVisionPipeline() {
+void PhotonVisionInterface::updatePhotonVision() {
     photonlib::PhotonPipelineResult latestPipelineResult = camera.GetLatestResult();
     bool hasTargets = latestPipelineResult.HasTargets();
 
@@ -64,12 +41,12 @@ void PhotonVisionInterface::updatePhotonVisionPipeline() {
         for (const auto& target : photonVisionTrackedTargets) {
             distances.push_back(static_cast<double>(photonlib::PhotonUtils::CalculateDistanceToTarget(
                 CAMERA_HEIGHT, 
-                PhotonVisionInterface::determineTargetHeight(target.GetFiducialId()), 
+                targetHeight[static_cast<int>(target.GetFiducialId())], 
                 CAMERA_PITCH, 
                 units::degree_t{target.GetPitch()})));
             yaws.push_back(static_cast<double>(target.GetYaw()));
             pitches.push_back(static_cast<double>(target.GetPitch()));
-            fiducialIds.push_back(static_cast<double>(target.GetFiducialId()));
+            fiducialIds.push_back(static_cast<int>(target.GetFiducialId()));
             poseAmbiguities.push_back(static_cast<double>(target.GetPoseAmbiguity()));
         }
     }
--- src/main/include/PhotonVisionInterface.h
+++ src/main/include/PhotonVisionInterface.h
@@ -1,12 +1,20 @@
 #pragma once
 #include <photonlib/PhotonUtils.h>
 #include <photonlib/PhotonCamera.h>
+#include <photonlib/PhotonPoseEstimator.h>
 #include <unordered_map>
-
+#include <vector>
+#include <iostream>
+#include <utility>
+#include <frc/apriltag/AprilTagFieldLayout.h>
+#include <frc/apriltag/AprilTagFields.h>
+#include <frc/geometry/Pose3d.h>
+ 
 class PhotonVisionInterface {
 public:
-    // This method should be called each tick of the Rio to send new data to Simulink
-    void updatePhotonVisionPipeline();
+    PhotonVisionInterface();
+    // This method should be called each tick of the Rio to update the data for each target given to simulink
+    void updatePhotonVision();
     // Methods to return the vectors
     /**
      * @brief Returns vector of the distance from each target to the camera.
@@ -28,22 +36,67 @@ public:
      * @brief Returns vector of confidence factors [0, 1] of the targets
     */
     const std::vector<double>& getPoseAmbiguities() const;
-
     /**
      * @brief Returns the vertical height of each target, needs to be setup for each game and senerio. 
     */
     const units::length::meter_t determineTargetHeight(int fiducialId) const;
+    /**
+     * @brief Prints out the current dected fiducialIds into console
+    */
+    const void printDetectedTargetIds() const;
+
+    /**
+     * @brief Returns the 3d pose of the robot in a vector (x, y, x)
+    */
+
+    /**
+     * @brief Returns the pose of the robot as a 3d pose
+    */
+
+    /**
+     * @brief Returns the 2d pose of the robot in a vector (x, y)
+    */
+
+    /**
+     * @brief Returns the pose of the robot as a 2d pose
+    */
 
 private:
+
+    // Makes a apriltag feild layout for our shop 
+    frc::AprilTag tag1 = frc::AprilTag(1, frc::Pose3d(frc::Translation3d(15.513558_m, 1.02743_m, 0.462788_m), frc::Rotation3d(0_rad, 0_rad, 0_rad)));
+    frc::AprilTag tag2 = frc::AprilTag(2, frc::Pose3d(frc::Translation3d(15.513558_m, 2.748026_m, 0.462788_m), frc::Rotation3d(0_rad, 0_rad, 0_rad)));
+    frc::AprilTag tag3 = frc::AprilTag(3, frc::Pose3d(frc::Translation3d(15.513558_m, 4.424426_m, 0.462788_m), frc::Rotation3d(0_rad, 0_rad, 0_rad)));
+    frc::AprilTag tag4 = frc::AprilTag(4, frc::Pose3d(frc::Translation3d(16.513558_m, 6.749796_m, 0.695452_m), frc::Rotation3d(0_rad, 0_rad, 0_rad)));
+    frc::AprilTag tag5 = frc::AprilTag(5, frc::Pose3d(frc::Translation3d(0.36195_m, 6.749796_m, 0.695452_m), frc::Rotation3d(0_rad, 0_rad, 0_rad)));
+    frc::AprilTag tag6 = frc::AprilTag(6, frc::Pose3d(frc::Translation3d(1.02743_m, 4.424426_m, 0.462788_m), frc::Rotation3d(0_rad, 0_rad, 0_rad)));
+    frc::AprilTag tag7 = frc::AprilTag(7, frc::Pose3d(frc::Translation3d(1.02743_m, 2.748026_m, 0.462788_m), frc::Rotation3d(0_rad, 0_rad, 0_rad)));
+    frc::AprilTag tag8 = frc::AprilTag(8, frc::Pose3d(frc::Translation3d(1.02743_m, 1.02743_m, 0.462788_m), frc::Rotation3d(0_rad, 0_rad, 0_rad)));
+
+    std::vector<frc::AprilTag> AprilTagList{tag1, tag2, tag3, tag4, tag5, tag6, tag7, tag8};
+    frc::AprilTagFieldLayout AprilTagFieldLayout = frc::AprilTagFieldLayout(AprilTagList, 16.54175_m, 8.0137_m);
+
     // Makes PhotonCamera Object
-    photonlib::PhotonCamera camera{"OV5647"};
+    photonlib::PhotonCamera camera{"OV5647"};    
+
     // Member variables to store data for each detected target
     std::vector<double> distances;
     std::vector<double> yaws;
     std::vector<double> pitches;
     std::vector<int> fiducialIds;
     std::vector<double> poseAmbiguities;
+
+    // A list of the target heights for finding distance to the targets
+    std::vector<units::length::meter_t> targetHeight{0_m, 0.462788_m, 0.462788_m, 0.462788_m, 0.695452_m, 0.695452_m, 0.462788_m, 0.462788_m, 0.462788_m}; 
+
+    //Robot Pose Estimator
+    frc::Transform3d robotToCamera = frc::Transform3d(frc::Translation3d(0.5_m, -0.25_m, CAMERA_HEIGHT), frc::Rotation3d(0_rad, CAMERA_PITCH, 0_rad));
+    /**
+     * @note I do not know exactly why I need std::move() in the constructor but it works so im running with it. Kill me if you want. *shrugs*
+    */
+    photonlib::PhotonPoseEstimator poseEstimator = photonlib::PhotonPoseEstimator(AprilTagFieldLayout, photonlib::MULTI_TAG_PNP, std::move(camera), robotToCamera);
+
     // Constants with preset values
     static constexpr units::meter_t CAMERA_HEIGHT{0.25}; // meters
-    static constexpr units::degree_t CAMERA_PITCH{0.0}; // degrees
+    static constexpr units::radian_t CAMERA_PITCH{0_rad}; // degrees (rad)
 };
