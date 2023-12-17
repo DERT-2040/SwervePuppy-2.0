9c88dd399c9bb5126f2188c3753460aba0e49170
JoshA <andersonjoshua578@gmail.com>
2023-12-16 23:57:40 -0600

    Janelyn - Merged all separate systems into one big system, curvature and position can now be called periodically

--- simulink/Unit_Tests/Catwell-Rom/Catwell_Rom_Plotting.m
+++ simulink/Unit_Tests/Catwell-Rom/Catwell_Rom_Plotting.m
@@ -1,12 +1,20 @@
 Pose = out.Robot_Pose(end, :);
-Knots = out.Spline_Knots(1:out.Num_Poses + 4 - out.Start_Index(end),:,end);
 Ref_Poses = [2 2; 4 3; 7 2; 8 0; 10 -2; 12 0; 15 -1; 16 -3; 13 -4; 13 -1];
+Data = out.ChainSampling.signals.values;
 
 figure(1), clf
 plot(Pose(1), Pose(2), 'o', 'MarkerFaceColor', 'k')
 hold on
-plot(Knots(:,1), Knots(:,2), 'g^-') 
-plot(Ref_Poses(1:out.Num_Poses, 1), Ref_Poses(1:out.Num_Poses, 2), 'ms-')
+plot(Ref_Poses(:, 1), Ref_Poses(:, 2), 'ms-')
 axis equal
 grid on
-legend('Robot', 'Spline Knots', 'Ref Poses')
+
+for i = 1:size(Data, 3)
+    plot(Data(:,1,i), Data(:,2,i), '-');
+end
+
+figure(2), clf
+hold on
+for i = 1:size(Data, 3)
+    plot((i-1:1/(Spline_Num_Samples-1):i)', Data(:, 3, i), '-');
+end
