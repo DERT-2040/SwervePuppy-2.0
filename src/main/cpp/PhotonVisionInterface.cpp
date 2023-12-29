ded8ee3ec97dfe18497d722b08336009f5a5d799
Janelyn Anderson <andersonjanelyn@gmail.com>
2023-12-28 19:59:12 -0600

    A finally working version of vision code that can find absolute position. Inaccuracy in the tag position and the field map exist so it is not perfect. If 2 tags in in view accuracy greatly increases. Simulink team can work on integrating measurements.

--- src/main/cpp/PhotonVisionInterface.cpp
+++ src/main/cpp/PhotonVisionInterface.cpp
@@ -1,30 +1,23 @@
 #include "include/PhotonVisionInterface.h"
 
 void PhotonVisionInterface::PreStep() {
-    auto globalPose = PhotonVisionInterface::GetEstimatedGlobalPose();
+    auto globalPose = photonEstimator.Update();
     if (globalPose.has_value()) {
         robot2dPose = globalPose->estimatedPose.ToPose2d();
+        frc::SmartDashboard::PutNumber("RobotX", static_cast<double>(robot2dPose.X()));
+        frc::SmartDashboard::PutNumber("RobotY", static_cast<double>(robot2dPose.Y()));
     }
 }
 
-void  PhotonVisionInterface::PostStep() {
+void PhotonVisionInterface::PostStep() {
 
 }
 
-void  PhotonVisionInterface::SmartDashboardCallback() {
-    frc::SmartDashboard::PutNumber("Robot X", static_cast<double>(robot2dPose.X()));
-    frc::SmartDashboard::PutNumber("Robot X", static_cast<double>(robot2dPose.Y()));
+void PhotonVisionInterface::SmartDashboardCallback() {
 }
 
-std::optional<photonlib::EstimatedRobotPose> PhotonVisionInterface::GetEstimatedGlobalPose() {
-    auto visionEst = photonEstimator.Update();
-    units::second_t latestTimestamp = camera.GetLatestResult().GetTimestamp();
-    bool newResult =
-    units::math::abs(latestTimestamp - lastEstTimestamp) > 1e-5_s;
-    if (newResult) {
-      lastEstTimestamp = latestTimestamp;
-    }
-    return visionEst;
+frc::Pose2d PhotonVisionInterface::GetEstimatedGlobalPose() {
+    return robot2dPose;
 }
 
  PhotonVisionInterface::PhotonVisionInterface() {
--- src/main/include/PhotonVisionInterface.h
+++ src/main/include/PhotonVisionInterface.h
@@ -8,6 +8,7 @@
 #include <frc/geometry/Pose2d.h>
 #include <frc/geometry/Transform3d.h>
 #include <frc/smartdashboard/SmartDashboard.h>
+#include <math.h>
 
 #include <memory>
 #include "units/time.h"
@@ -18,14 +19,27 @@ class PhotonVisionInterface {
     void PostStep();
     void SmartDashboardCallback();
     PhotonVisionInterface();
-    std::optional<photonlib::EstimatedRobotPose> GetEstimatedGlobalPose();
+    frc::Pose2d PhotonVisionInterface::GetEstimatedGlobalPose();
 
   private:
+  double m_pi = 3.1415926535897;
+  // Makes a apriltag feild layout for our shop 
+    frc::AprilTag tag2 = frc::AprilTag(2, frc::Pose3d(frc::Translation3d(2.032_m, 4.5212_m, 0.4953_m), frc::Rotation3d(0_rad, 0_rad, 4.71238898_rad))); 
+    frc::AprilTag tag3 = frc::AprilTag(3, frc::Pose3d(frc::Translation3d(5.207_m, 2.8702_m, 1.1811_m), frc::Rotation3d(0_rad, 0_rad, 3.1415926535897_rad)));
+    frc::AprilTag tag4 = frc::AprilTag(4, frc::Pose3d(frc::Translation3d(5.207_m, 1.3462_m, 0.8382_m), frc::Rotation3d(0_rad, 0_rad, 3.1415926535897_rad)));
+    frc::AprilTag tag5 = frc::AprilTag(5, frc::Pose3d(frc::Translation3d(3.8354_m, 0_m, 0.8763_m), frc::Rotation3d(0_rad, 0_rad, 1.570796327_rad))); 
+    frc::AprilTag tag6 = frc::AprilTag(6, frc::Pose3d(frc::Translation3d(1.1684_m, 1.8542_m, 0.2032_m), frc::Rotation3d(0_rad, 0_rad, 0_rad)));
+    frc::AprilTag tag7 = frc::AprilTag(7, frc::Pose3d(frc::Translation3d(1.778_m, 0_m, 0.4953_m), frc::Rotation3d(0_rad, 0_rad, 1.570796327_rad)));
+    frc::AprilTag tag8 = frc::AprilTag(8, frc::Pose3d(frc::Translation3d(-0.6858_m, 4.0894_m, 0.1778_m), frc::Rotation3d(0_rad, 0_rad, 0_rad)));
+
+    std::vector<frc::AprilTag> AprilTagList{tag2, tag3, tag4, tag5, tag6, tag7, tag8};
+    frc::AprilTagFieldLayout AprilTagFieldLayout = frc::AprilTagFieldLayout(AprilTagList, 4.5212_m, 4.572_m);
+
   const frc::Transform3d robotToCam{
-        frc::Translation3d{0.5_m, 0.0_m, 0.5_m},
+        frc::Translation3d{0.254_m, 0.0508_m, 0.1524_m},
         frc::Rotation3d{0_rad, 0_rad, 0_rad}};
   photonlib::PhotonPoseEstimator photonEstimator{
-      LoadAprilTagLayoutField(frc::AprilTagField::k2023ChargedUp),
+      AprilTagFieldLayout,
       photonlib::PoseStrategy::MULTI_TAG_PNP,
       photonlib::PhotonCamera{"OV5647"}, robotToCam};
 
