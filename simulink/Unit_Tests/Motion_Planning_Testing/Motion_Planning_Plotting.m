aa3f31c992e4dd26d48b99d1005c0880de2389f4
Janelyn Anderson <andersonjanelyn@gmail.com>
2023-12-31 19:21:47 -0600

    Small Changes

--- simulink/Unit_Tests/Motion_Planning_Testing/Motion_Planning_Plotting.m
+++ simulink/Unit_Tests/Motion_Planning_Testing/Motion_Planning_Plotting.m
@@ -1,5 +1,6 @@
 Data = out.Motion_Plan_Out;
 Maxes = out.Maxes;
+Num_Poses = out.Num_Poses;
 Num_Maxes = out.Num_Maxes;
 Positions = Data(:, 1:2);
 Curvatures = Data(:, 3);
@@ -8,10 +9,10 @@ tcl = tiledlayout(1,2,'TileSpacing','compact');
 
 nexttile
     
-plot(Positions(:,1), Positions(:,2));
+plot(Positions(1:Spline_Num_Samples*Num_Poses,1), Positions(1:Spline_Num_Samples*Num_Poses,2));
 
 nexttile
 hold on;
-plot(1:Spline_Num_Samples*(Spline_Max_Num_RefPoses-1), Curvatures(:,1));
+plot(1:Spline_Num_Samples*(Num_Poses-1), Curvatures(1:Spline_Num_Samples*(Num_Poses-1),1));
 
 scatter(Maxes(1:Num_Maxes, 1), Maxes(1:Num_Maxes, 2));
